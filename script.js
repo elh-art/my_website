bb1fc12ba46868461095715492da77a393113fb0
elh-art <code.enfaced@aleeas.com>
2021-12-12 21:09:35 +0100

    progress

---
+++ script.js
@@ -0,0 +1,12 @@
+const boxes = document.querySelectorAll('.box')
+
+// console.log(boxes)
+
+// boxes.forEach((e) => {
+//   e.addEventListener('mouseover', (e) => {
+//     e.target.classList.add('flip')
+//   })
+//   e.addEventListener('mouseout', (e) => {
+//     e.target.classList.remove('flip')
+//   })
+// })
